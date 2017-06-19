ca355f43f975fa82d9f8f43cf562854a310a9281
Matheus Valeriano <matheus.valeriano@wpensar.com.br>
2017-06-19 09:53:06 -0300

    add another test

--- test/replaceTagMultipleValues.test.js
+++ test/replaceTagMultipleValues.test.js
@@ -16,17 +16,26 @@ describe('replaceTagsDistinctValues()', () => {
     expect(preProc.replaceTagsDistinctValues(replacements, 'aaa/*[(\\a\\b)]*/xxx/*[/(\\a\\b)]*/bbb')).to.equal('aaa@bla@bbb');
   });
 
-  it('should replace specific tags in multiple formats', () => {
-    var replacements = [
-      {tag: 'TAG1', replacement: '@foo@'},
-      {tag: 'TAG3', replacement: '@foo3@'},
-      {tag: 'TAG4', replacement: '@foo4@'},
-      {tag: 'TAG(5)', replacement: '@foo(5)@'},
-      {tag: 'TAG[6]', replacement: '@foo[6]@'},
-      {tag: 'TAG8', replacement: '@foo8@'},
-      {tag: 'TAG9', replacement: '@foo9@'},
-    ];
-    expect(preProc.replaceTagsDistinctValues(replacements,
+it('should replace array tags with distinct array of replacements and unique tag with your distinct value', () => {
+	var replacements = [
+    {tag: ['a!b', 'tag'], replacement: ['@foo@', '@foo2@']},
+    {tag: 'a b $', replacement: '@bar@'},
+  ];
+	var content = 'aaa/*[a!b]*/xxx/*[/a!b]*/bbb/*[tag]*/yyy/*[/tag]*/ccc/*[a b $]*/zzz/*[/a b $]*/';
+  expect(preProc.replaceTagsDistinctValues(replacements, content)).to.equal('aaa@foo@bbb@foo2@ccc@bar@');
+});
+
+it('should replace specific tags in multiple formats', () => {
+  var replacements = [
+    {tag: 'TAG1', replacement: '@foo@'},
+    {tag: 'TAG3', replacement: '@foo3@'},
+    {tag: 'TAG4', replacement: '@foo4@'},
+    {tag: 'TAG(5)', replacement: '@foo(5)@'},
+    {tag: 'TAG[6]', replacement: '@foo[6]@'},
+    {tag: 'TAG8', replacement: '@foo8@'},
+    {tag: 'TAG9', replacement: '@foo9@'},
+  ];
+  expect(preProc.replaceTagsDistinctValues(replacements,
       `foo bar1
 // [TAG1]
 foo bar2
