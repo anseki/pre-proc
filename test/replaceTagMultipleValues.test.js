c1b413e59730cfc7db7e4d20f2129d37cfa6023d
Matheus Valeriano <matheus.valeriano@wpensar.com.br>
2017-06-19 09:29:43 -0300

    add tests

--- test/replaceTagMultipleValues.test.js
+++ test/replaceTagMultipleValues.test.js
@@ -3,68 +3,30 @@
 const expect = require('chai').expect,
   preProc = require('../lib/pre-proc');
 
-describe.only('replaceTagMultipleValues()', () => {
+describe('replaceTagsDistinctValues()', () => {
 
-  it('should replace specific tag', () => {
-    expect(preProc.replaceTagMultipleValues([{tag: 'tag', replacement: '@foo@'}], 'aaa/*[tag]*/xxx/*[/tag]*/bbb')).to.equal('aaa@foo@bbb');
-  });
-
-  it('should replace a tag that contains meta characters', () => {
+  it('should replace tags those contains meta characters', () => {
     var replacements = [
       {tag: 'a!b', replacement: '@foo@'},
       {tag: 'a b $', replacement: '@bar@'},
       {tag: '(\\a\\b)', replacement: '@bla@'}
     ];
-    expect(preProc.replaceTagMultipleValues(replacements, 'aaa/*[a!b]*/xxx/*[/a!b]*/bbb')).to.equal('aaa@foo@bbb');
-    expect(preProc.replaceTagMultipleValues(replacements, 'aaa/*[a b $]*/xxx/*[/a b $]*/bbb')).to.equal('aaa@bar@bbb');
-    expect(preProc.replaceTagMultipleValues(replacements, 'aaa/*[(\\a\\b)]*/xxx/*[/(\\a\\b)]*/bbb')).to.equal('aaa@bla@bbb');
-  });
-  
-
-  it('should replace only specific tags', () => {
-    var replacements = [
-      {tag: 'TAG1', replacement: '@foo@'},
-      {tag: 'TAG3', replacement: '@foo@'}
-    ];
-    expect(preProc.replaceTagMultipleValues(replacements,
-      `foo bar1
-// [TAG1]
-foo bar2
-foo bar3
-// [/TAG1]
-xxx
-// [TAG2]
-foo bar4
-foo bar5
-// [/TAG2]
-xxx
-// [TAG3]
-foo bar6
-foo bar7
-// [/TAG3]
-`)).to.equal(`foo bar1
-@foo@
-xxx
-// [TAG2]
-foo bar4
-foo bar5
-// [/TAG2]
-xxx
-@foo@
-`);
+    expect(preProc.replaceTagsDistinctValues(replacements, 'aaa/*[a!b]*/xxx/*[/a!b]*/bbb')).to.equal('aaa@foo@bbb');
+    expect(preProc.replaceTagsDistinctValues(replacements, 'aaa/*[a b $]*/xxx/*[/a b $]*/bbb')).to.equal('aaa@bar@bbb');
+    expect(preProc.replaceTagsDistinctValues(replacements, 'aaa/*[(\\a\\b)]*/xxx/*[/(\\a\\b)]*/bbb')).to.equal('aaa@bla@bbb');
   });
 
   it('should replace specific tags in multiple formats', () => {
     var replacements = [
       {tag: 'TAG1', replacement: '@foo@'},
-      {tag: 'TAG3', replacement: '@foo@'},
-      {tag: 'TAG4', replacement: '@foo@'},
-      {tag: 'TAG(5)', replacement: '@foo@'},
-      {tag: 'TAG[6]', replacement: '@foo@'},
-      {tag: 'TAG8', replacement: '@foo@'},
-      {tag: 'TAG9', replacement: '@foo@'},
+      {tag: 'TAG3', replacement: '@foo3@'},
+      {tag: 'TAG4', replacement: '@foo4@'},
+      {tag: 'TAG(5)', replacement: '@foo(5)@'},
+      {tag: 'TAG[6]', replacement: '@foo[6]@'},
+      {tag: 'TAG8', replacement: '@foo8@'},
+      {tag: 'TAG9', replacement: '@foo9@'},
     ];
-    expect(preProc.replaceTagMultipleValues(replacements,
+    expect(preProc.replaceTagsDistinctValues(replacements,
       `foo bar1
 // [TAG1]
 foo bar2
@@ -106,20 +68,20 @@ foo bar4
 foo bar5
 // [/TAG2]
 xxx
-@foo@
+@foo3@
 foo bar8
-@foo@
-fo@foo@ara
+@foo4@
+fo@foo(5)@ara
 foo barb
-foo   @foo@c
+foo   @foo[6]@c
 foo bard
 <!--    [TAG7]  -->
 foo bare
 <!--    [/TAG7]  -->
 foo barf
-@foo@
+@foo8@
 foo barh
-foo bari@foo@
+foo bari@foo9@
 foo barl
 `);
   });
