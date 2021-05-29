8eba1d323366eef017fe6373ca67d7f72c4edb28
anseki <AnSeki.aff@gmail.com>
2021-05-29 21:46:39 +0900

    Update for mocha@8

---
+++ .mocharc.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+  spec: [
+    './test/isTargetPath.test.js',
+    './test/replaceTag.test.js',
+    './test/removeTag.test.js',
+    './test/pickTag.test.js'
+  ]
+};
